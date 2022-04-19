ae8683ac9ca725d871e59d9883a590da120108fc
BlueMush <gusah0909@naver.com>
2022-04-19 17:19:24 +0900

    함수 disabled 하기

--- src/test/java/com/example/demo/Study1Test.java
+++ src/test/java/com/example/demo/Study1Test.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class Study1Test {
@@ -17,6 +18,7 @@ public class Study1Test {
 	}
 	
 	@Test
+	@Disabled
 	void create1() {
 		System.out.println("create1");
 	}
