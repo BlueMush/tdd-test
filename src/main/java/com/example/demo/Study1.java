93624cf30ce5a003ad4861a17bcc0a1abc6700a6
BlueMush <gusah0909@naver.com>
2022-04-19 18:25:23 +0900

    assertAll Test

--- src/main/java/com/example/demo/Study1.java
+++ src/main/java/com/example/demo/Study1.java
@@ -1,15 +1,15 @@
 package com.example.demo;
 
+import lombok.Getter;
+
+@Getter
 public class Study1 {
 	
 	private StudyStatus status = StudyStatus.DRAFT;
+	private int limit = 1;
 	
 
 	public StudyStatus getStatus() {
 		return this.status;
-	}
-	
-	void create() {
-		Study1 study = new Study1();
 	}
 }
--- src/test/java/com/example/demo/Study3Assert.java
+++ src/test/java/com/example/demo/Study3Assert.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -13,8 +15,14 @@ public class Study3Assert {
 	@Test
 	void create() {
 		Study1 study = new Study1();
-		assertNotNull(study);
-		assertEquals(StudyStatus.DRAFT, study.getStatus(), ()->"스터디 처음 만들면 상태값이 " + StudyStatus.DRAFT);
+		assertAll(
+			()-> assertNotNull(study),
+			()->assertEquals(StudyStatus.DRAFT, study.getStatus(), ()->"스터디 처음 만들면 상태값이 " + StudyStatus.DRAFT),
+			()->assertTrue(study.getLimit() > 0, ()-> " 최대인원은 0보다 커야된다")
+		);
+//		assertNotNull(study);
+//		assertEquals(StudyStatus.DRAFT, study.getStatus(), ()->"스터디 처음 만들면 상태값이 " + StudyStatus.DRAFT);
+//		assertTrue(study.getLimit() > 0, ()-> " 최대인원은 0보다 커야된다");
 	}
 	
 	@BeforeAll
