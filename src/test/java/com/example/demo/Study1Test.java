0e1841bd20fd571f23858f19832bf719cc5e9808
BlueMush <gusah0909@naver.com>
2022-04-19 17:13:35 +0900

    test

--- src/test/java/com/example/demo/Study1Test.java
+++ src/test/java/com/example/demo/Study1Test.java
@@ -1,44 +1,36 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 public class Study1Test {
-	@Test
-	void create() {
-		Study1 study = new Study1();
-		assertNotNull(study);
-		System.out.println("create");
-	}
-	
-	@Test
-	void create1() {
-		System.out.println("create1");
-	}
-	
-	@BeforeAll
-	static void beforeAll() {
-		System.out.println("before all");
-	}
-	
-	
-	@AfterAll
-	static void afterAll() {
-		System.out.println("after all");
-	}
-	
-	@BeforeEach
-	void beforeEach() {
-		System.out.println("before each");
-	}
-	
-	@AfterEach
-	void afterEach() {
-		System.out.println("after each");
-	}
+//	@Test
+//	void create() {
+//		Study1 study = new Study1();
+//		assertNotNull(study);
+//		System.out.println("create");
+//	}
+//	
+//	@Test
+//	void create1() {
+//		System.out.println("create1");
+//	}
+//	
+//	@BeforeAll
+//	static void beforeAll() {
+//		System.out.println("before all");
+//	}
+//	
+//	
+//	@AfterAll
+//	static void afterAll() {
+//		System.out.println("after all");
+//	}
+//	
+//	@BeforeEach
+//	void beforeEach() {
+//		System.out.println("before each");
+//	}
+//	
+//	@AfterEach
+//	void afterEach() {
+//		System.out.println("after each");
+//	}
 }
