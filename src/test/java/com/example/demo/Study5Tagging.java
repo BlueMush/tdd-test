789f9eadd0342cd2c482e31d9134a64b46097ef6
BlueMush <gusah0909@naver.com>
2022-04-21 14:03:39 +0900

    태깅 Test

---
+++ src/test/java/com/example/demo/Study5Tagging.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class Study5Tagging {
+	@Test
+	@DisplayName("еб╠К ©╛╫ю")
+	@Tag("fast")
+	void new_test1() {
+		System.out.println("fast");
+	}
+	
+	@Test
+	@DisplayName("еб╠К ©╛╫ю")
+	@Tag("slow")
+	void new_test2() {
+		System.out.println("slow");
+	}
+}
