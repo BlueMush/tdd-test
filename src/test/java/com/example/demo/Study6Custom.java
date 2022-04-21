c1b2dbcb1ba6b2be13fe3a421e8a9f3ad1b6ab8f
BlueMush <gusah0909@naver.com>
2022-04-21 14:15:14 +0900

    커스텀 태그 테스트

---
+++ src/test/java/com/example/demo/Study6Custom.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Study6Custom {
+	@Test
+	@DisplayName("커스텀 태그 만들기")
+	@CustomFast
+	void new_test() {
+		System.out.println("커스텀 태그 테스트");
+	}
+}
