f27bff0fc206f4ac776bfd87ad5a98c66752f810
BlueMush <gusah0909@naver.com>
2022-04-21 16:32:21 +0900

    junit 확장모델 사용

---
+++ src/test/java/com/example/demo/Study10Test.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(FindSlowTestExtension.class)
+public class Study10Test {
+int value = 1;
+	
+	@Order(2)
+	@CustomFast
+	@Test
+	@DisplayName("먼저 실행")
+	void new_test1() {
+		System.out.println("2 = " + value++);
+	}
+	
+	@Order(1)
+	@SlowTest
+	@DisplayName("이게 더 먼저 실행")
+	void new_test2() throws InterruptedException {
+		Thread.sleep(1005L);
+		System.out.println("1 = " + value++);
+	}
+}
